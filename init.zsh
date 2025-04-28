2caa20d3ee545d3d1114ae85bbcfda96ac9bd4db
Jason L Connor <jason.connor@gmail.com>
2025-04-28 14:52:32 -0600

    Generate completions

--- init.zsh
+++ init.zsh
@@ -1,3 +1,15 @@
+(( ${+commands[op]} || ${+commands[asdf]} && ${+functions[_direnv_hook]} )) && () {
+
+  local command=${commands[op]:-"$(${commands[asdf]} which op 2> /dev/null)"}
+  [[ -z $command ]] && return 1
+
+  local compfile=$1/functions/_op
+  if [[ ! -e $compfile || $compfile -ot $command ]]; then
+    $command completion zsh >| $compfile
+    print -u2 -PR "* Detected a new version 'op'. Regenerated completions."
+  fi
+} ${0:h}
+
 export ANTHROPIC_API_KEY=op://Personal/Anthropic/credential
 export OPENAI_API_KEY=op://Personal/OpenAI/credential
 export OPENROUTER_API_KEY=op://Personal/OpenRouter/credential
